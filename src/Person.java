fade0af5a687839562dfb3053d79debcf457cb48
10011999 <motewaraditya77@gmail.com>
2022-09-17 13:15:28 +0530

    Added Person Details

---
+++ src/Person.java
@@ -0,0 +1,19 @@
+public class Person {
+    String firstName;
+    String lastName;
+    String address;
+    String city;
+    String email;
+    int zip;
+    long phoneNumber;
+
+    public Person(String firstName, String lastName, String address, String city, String email, int zip, long phoneNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.email = email;
+        this.zip = zip;
+        this.phoneNumber = phoneNumber;
+    }
+}
